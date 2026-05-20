43733c2c3e2eeceea8c92b896e95903c09155839
JMurillo <jorgemurilloc@est.uepmrmp.edu.ec>
2026-05-20 09:54:26 -0500

    Add files via upload

---
+++ Unidad1/Variables/Variables.js
@@ -0,0 +1,9 @@
+//let: para variables que no cambian, const: Variable que nunca va a cambiar, Var: Una mezcla de ambos.
+let nombre = "Jorge Enrique Murillo Cabeza"
+//let valor a la variable
+console.log(nombre)
+//console.log(nombre variable): Para imprimir con consola.
+const val1 = 1
+//const: valor constante y no puede ser cambiado.
+console.log(val1)
+
